ae5c9db8cdb7836d33b92ff2e2d2e3d6f0637e91
licb <545360100@qq.com>
2019-07-08 21:35:09 +0800

    深拷贝 每日一题

---
+++ 题目/实现一个深拷贝.js
@@ -0,0 +1,26 @@
+function deepClone(obj, hash = new WeakMap()) {
+  if (obj instanceof RegExp) {
+    return new RegExp(obj);
+  }
+  if (obj instanceof Date) {
+    return new Date(obj);
+  }
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+  if (hash.has(obj)) {
+    return hash.get(obj);
+  }
+  /**
+   * 如果obj是数组，那么obj.constructor是[Function:Array]
+   * 如果obj是对象，那么obj.constructor是[Function:Object]
+   */
+  let t = new obj.constructor();
+  hash.set(obj, t);
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      t[key] = deepClone(obj[key], hash);
+    }
+  }
+  return t;
+}
