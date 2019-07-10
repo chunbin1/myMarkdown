2bb4dc1d86e8bf449738b74948365d0959208d98
licb <545360100@qq.com>
2019-07-10 09:21:41 +0800

    快速排序

---
+++ 数据结构/排序/快速排序.js
@@ -0,0 +1,22 @@
+function quickSort(arr) {
+  if (arr.length<=1) {return arr;}
+  const left = [],
+    right = [],
+    baseDot =Math.round(arr.length/2),
+    base =arr.splice(baseDot, 1)[0];
+
+  for (var i =0; i <arr.length; i++) {
+    if (arr[i] < base) {
+      left.push(arr[i])
+    }else {
+      right.push(arr[i])
+    }
+  }
+
+  return [...quickSort(left),base,...quickSort(right)];
+}
+
+const arr = [9,23,4,56,7,7,31,23]
+console.time('sort')
+console.log(quickSort(arr))
+console.timeEnd('sort')
